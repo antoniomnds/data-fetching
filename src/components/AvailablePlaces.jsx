b9971c6ce32af99acf884f585ad562a41ee73d15
Antonio Santos <antoniomndsantos@gmail.com>
2024-12-17 16:28:27 +0000

    Fetch places from backend

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,12 +1,30 @@
 import Places from './Places.jsx';
+import {useEffect, useState} from "react";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching, setIsFetching] = useState(false);
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+
+  useEffect(() => {
+    async function fetchPlaces() {
+      setIsFetching(true);
+      const response = await fetch('http://localhost:3000/places');
+      const responseData = await response.json();
+      setAvailablePlaces(responseData.places);
+      setIsFetching(false)
+    }
+
+    fetchPlaces();
+  }, []);
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
+      loadingText="Fetching place data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
+      isLoading={isFetching}
     />
   );
 }
