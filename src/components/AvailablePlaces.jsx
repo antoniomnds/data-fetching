493d38a6abe5143bbf4a69cbb2e9a1a2ef58f7a8
Antonio Santos <antoniomndsantos@gmail.com>
2024-12-18 10:47:34 +0000

    Sort available places by distance to the current user

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,6 +1,7 @@
 import Places from './Places.jsx';
 import {useEffect, useState} from "react";
 import ErrorPage from "./Error.jsx";
+import {sortPlacesByDistance} from "../loc.js";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false); // loading state
@@ -19,14 +20,19 @@ export default function AvailablePlaces({ onSelectPlace }) {
           throw new Error('Failed to fetch places.')
         }
         const responseData = await response.json();
-        setAvailablePlaces(responseData.places);
+
+        navigator.geolocation.getCurrentPosition(position => {
+          const {latitude, longitude} = position.coords;
+          const sortedPlaces = sortPlacesByDistance(responseData.places, latitude, longitude);
+          setAvailablePlaces(sortedPlaces);
+          setIsFetching(false);
+        })
       } catch (error) {
         setError({
           message: error.message || "Could not fetch places, please try again later."
         });
+        setIsFetching(false);
       }
-
-      setIsFetching(false); // stops fetching either way
     }
 
     fetchPlaces();
